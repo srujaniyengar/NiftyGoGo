90892e1784f4721848a751b7ca758654106ef81f
srujaniyengar <srujanparthasarathyiyengar@gmail.com>
2025-02-22 20:30:23 +0530

    fix: other.go

--- utils/other.go
+++ utils/other.go
@@ -1,14 +1,15 @@
 package utils  
 import "fmt"  
+
 /** 
  * THIS MOD IS ONLY FOR CONVENIENT USE OF EXCEPTION HANDLING.  
  * THIS FUNCTION TAKES ANY ERROR AND PRINTS AN ERROR MESSAGE IF THE PROVIDED VALUE IS NOT NIL.  
  * THIS PREVENTS REDUNDANT ERROR CHECKING THROUGHOUT THE CODE.  
  */
-func Check(err any) {
+func Check(err any) (map_flag any, error_flag any) {
     if err != nil {
         fmt.Println(err, "Error")
-        return
+        return nil, err
     }
+    return true, nil  // Return default values when no error
 }
-
